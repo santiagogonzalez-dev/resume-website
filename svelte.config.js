b2f1d3cf42ef97fb55b4d1515b29dad6e920ebf2
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-06-07 20:35:26 -0300

    test: Added path for gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,8 +8,12 @@ const config = {
 		adapter: adapter(),
 		prerender: {
 			entries: ['/[[lang]]', '/en', '/es']
+		},
+		paths: {
+			base:
+				process.env.NODE_ENV === 'production' ? '/resume-website' : ''
 		}
 	}
-};
+}
 
-export default config;
+export default config
